25ce76986f47c5fb2712f0c1e92dcf290cc59d44
bk62 <bkafle862@gmail.com>
2021-06-03 12:07:07 +0545

    Updated gitignore and formatting fixes.

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-# https://www.freecodecamp.org/news/create-a-discord-bot-with-python/
-
-import discord
 from discord.ext import commands
-import os
-from replit import db
 from keep_alive import keep_alive
 import logging
 
 import settings
 
-
 logging.basicConfig(level=logging.INFO)
-
 
 bot = commands.Bot(command_prefix=settings.COMMAND_PREFIX, case_insensitive=True)
 
@@ -31,12 +24,9 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
-
-
 keep_alive()
 
-
 for extension in settings.EXTENSIONS:
-  bot.load_extension(extension)
+    bot.load_extension(extension)
 
 bot.run(settings.TOKEN)
