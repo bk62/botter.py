f327e0702d9367569fff907c74b3e6344f7dfdef
bk62 <bkafle862@gmail.com>
2021-06-05 13:50:34 +0545

    DB related updates to CLI commands and extension loading
    Import models package/module from available extension if they exist
    Add db related commands to run.py
    Can now init, clear and re-init db

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 import logging
-
+import importlib
 import settings
 
 logging.basicConfig(level=logging.INFO)
@@ -8,7 +8,6 @@ logging.basicConfig(level=logging.INFO)
 bot = commands.Bot(command_prefix=settings.COMMAND_PREFIX, case_insensitive=True)
 
 bot.author_id = int(settings.BOT_OWNER_ID)
-
 logging.info(f'Setting bot author id to {settings.BOT_OWNER_ID}')
 
 
@@ -28,9 +27,24 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
-def main():
+def _import_models(package):
+    try:
+        importlib.import_module(f"{package}.models")
+    except ModuleNotFoundError:
+        pass
+    except ImportError:
+        logging.debug(f'Could not import `{package}.models`')
+        return
+
+
+def init_extensions():
     for extension in settings.EXTENSIONS:
+        _import_models(extension)
         bot.load_extension(extension)
+
+
+def main():
+    init_extensions()
 
     bot.run(settings.TOKEN)
 
--- run.py
+++ run.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python3
+import asyncio
 from keep_alive import keep_alive as flask_keep_alive
 import main
+import db
 import click
 
 
-@click.command()
-@click.option('--keep-alive', default=False, help='Run flask thread to keep Repl alive.')
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('--keep-alive', is_flag=True, help='Run flask thread to keep Repl alive.')
 def run(keep_alive):
     """Run the Discord bot."""
     if keep_alive:
@@ -13,5 +21,34 @@ def run(keep_alive):
     main.main()
 
 
+async def _run_db(init=True, drop=False):
+    main.init_extensions()
+    async with db.engine.begin() as conn:
+        if drop:
+            click.echo('Dropping...')
+            await conn.run_sync(db.Base.metadata.drop_all)
+        if init:
+            click.echo('Creating...')
+            await conn.run_sync(db.Base.metadata.create_all)
+
+
+@cli.command('initdb')
+def initdb():
+    """Create all tables."""
+    asyncio.run(_run_db())
+
+
+@cli.command('dropdb')
+def dropdb():
+    """Drop all tables."""
+    asyncio.run(_run_db(init=False, drop=True))
+
+
+@cli.command('resetdb')
+def resetdb():
+    """Reset all tables."""
+    asyncio.run(_run_db(init=True, drop=True))
+
+
 if __name__ == '__main__':
-    run()
+    cli()
